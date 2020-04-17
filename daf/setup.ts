9220b0d1061a9a2f1ea7a481c9bcb0eceb0f8964
Simonas Karuzas <simonas@not.cat>
2020-04-17 18:01:57 +0300

    fix: Reusing existing DB connection

--- daf/setup.ts
+++ daf/setup.ts
@@ -8,7 +8,7 @@ import * as DafEthrDid from 'daf-ethr-did'
 import { KeyManagementSystem } from 'daf-libsodium'
 import { DafResolver } from 'daf-resolver'
 import * as DIDComm from 'daf-did-comm'
-import { createConnection } from 'typeorm'
+import { createConnection, Connection, getConnection } from 'typeorm'
 
 const DATABASE_URL = process.env.DATABASE_URL || null
 const commonConfig = {
@@ -28,8 +28,16 @@ const localConfig = {
 }
 const config = DATABASE_URL ? serverConfig : localConfig
 
-// @ts-ignore -> It's looking for a driver for expo?
-const dbConnection = createConnection(config)
+const getOrCreateDbConnection = async (): Promise<Connection> => {
+  try {
+    return getConnection()
+  } catch (e) {
+    // @ts-ignore -> It's looking for a driver for expo?
+    return createConnection(config)
+  }
+} 
+
+const dbConnection = getOrCreateDbConnection()
 const infuraProjectId = '5ffc47f65c4042ce847ef66a3fa70d4c'
 let didResolver = new DafResolver({ infuraProjectId })
 const rinkebyIdentityProvider = new DafEthrDid.IdentityProvider({
